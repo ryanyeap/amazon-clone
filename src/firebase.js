ede82f7ca599b17344f17b8f681c0ab6bed961b4
Ryan Yeap <ryanyeap@hotmail.com>
2020-09-21 19:42:58 +0800

    removed firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAisRWeZZpKQwKyeeEhT8RX8PxlSdamAo0",
-  authDomain: "clone-7ba26.firebaseapp.com",
-  databaseURL: "https://clone-7ba26.firebaseio.com",
-  projectId: "clone-7ba26",
-  storageBucket: "clone-7ba26.appspot.com",
-  messagingSenderId: "754350842815",
-  appId: "1:754350842815:web:305f30c25f7c4579d3770a",
-  measurementId: "G-FMXLQ1PSTK",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
